75d1862ff72d009dd5f494b0e496b0657ef21072
Eddie Won <dev@ediwon.com>
2025-03-14 06:45:19 +1000

    oh-my-zsh install and config fix

--- oh-my-zsh-install.sh
+++ oh-my-zsh-install.sh
@@ -12,16 +12,8 @@ git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-m
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 NEW_PLUGINS="zsh-autosuggestions zsh-syntax-highlighting"
 
-
-# 检查 ~/.zshrc 是否已有 plugins 配置
-if grep -q "^plugins=(" ${CONFIG_FILE}; then
-    # 替换现有的 plugins 行
-    sed -i "s/^plugins=(.*)/plugins=($NEW_PLUGINS)/" ${CONFIG_FILE}
-else
-    # 如果没有 plugins 配置，则添加
-    echo "plugins=($NEW_PLUGINS)" >> ${CONFIG_FILE}
-fi
-
+# 替换现有的 plugins 行
+sed -i "s/^plugins=(.*)/plugins=($NEW_PLUGINS)/" ${CONFIG_FILE}
 #  reload config
 echo 'source....'
-source ${CONFIG_FILE}
+zsh -c "source ~/.zshrc"
