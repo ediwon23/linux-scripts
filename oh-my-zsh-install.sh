faa1b73292d1fe9c24212f2b56ec3351355b7bd4
Eddie Won <dev@ediwon.com>
2025-03-14 06:39:42 +1000

    oh-my-zsh install and config fix

--- oh-my-zsh-install.sh
+++ oh-my-zsh-install.sh
@@ -5,7 +5,7 @@ sh -c "$(curl -fsSL https://install.ohmyz.sh/)"
 # configure for theme
 CONFIG_FILE="${HOME}/.zshrc"
 NEW_THEME="dst"
-sed -i '' "s/^ZSH_THEME=\".*\"/ZSH_THEME=\"$NEW_THEME\"/" ${CONFIG_FILE}
+sed -i "s/^ZSH_THEME=\".*\"/ZSH_THEME=\"$NEW_THEME\"/" ${CONFIG_FILE}
 
 # 要添加的插件列表
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
@@ -16,7 +16,7 @@ NEW_PLUGINS="zsh-autosuggestions zsh-syntax-highlighting"
 # 检查 ~/.zshrc 是否已有 plugins 配置
 if grep -q "^plugins=(" ${CONFIG_FILE}; then
     # 替换现有的 plugins 行
-    sed -i '' "s/^plugins=(.*)/plugins=($NEW_PLUGINS)/" ${CONFIG_FILE}
+    sed -i "s/^plugins=(.*)/plugins=($NEW_PLUGINS)/" ${CONFIG_FILE}
 else
     # 如果没有 plugins 配置，则添加
     echo "plugins=($NEW_PLUGINS)" >> ${CONFIG_FILE}
