bcbb014ec87132794701d736c535b35d0656b264
Eddie Won <dev@ediwon.com>
2025-03-26 01:04:14 +1000

    fix interupt after installed oh-my-zsh

--- oh-my-zsh-install.sh
+++ oh-my-zsh-install.sh
@@ -1,20 +1,31 @@
+#!/bin/bash
+
+# 更新系统并安装 zsh
 sudo apt update
-sudo apt install zsh -y
-# install oh-my-zsh
-sh -c "$(curl -fsSL https://install.ohmyz.sh/)"
-# configure for theme
+sudo apt install -y zsh
+
+# 安装 oh-my-zsh（不会中断）
+RUNZSH=no sh -c "$(curl -fsSL https://install.ohmyz.sh/)"
+
+# 配置主题
 CONFIG_FILE="${HOME}/.zshrc"
 NEW_THEME="dst"
-sed -i "s/^ZSH_THEME=\".*\"/ZSH_THEME=\"$NEW_THEME\"/" ${CONFIG_FILE}
+sed -i "s/^ZSH_THEME=\".*\"/ZSH_THEME=\"$NEW_THEME\"/" "${CONFIG_FILE}"
 
-# 要添加的插件列表
-git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+# 安装插件
+ZSH_CUSTOM="${ZSH_CUSTOM:-~/.oh-my-zsh/custom}"
+git clone https://github.com/zsh-users/zsh-autosuggestions "${ZSH_CUSTOM}/plugins/zsh-autosuggestions"
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git "${ZSH_CUSTOM}/plugins/zsh-syntax-highlighting"
+
+# 设置插件
 NEW_PLUGINS="git zsh-autosuggestions zsh-syntax-highlighting"
+sed -i "s/^plugins=(.*)/plugins=(${NEW_PLUGINS})/" "${CONFIG_FILE}"
 
-# 替换现有的 plugins 行
-sed -i "s/^plugins=(.*)/plugins=($NEW_PLUGINS)/" ${CONFIG_FILE}
-#  reload config
+# 设置默认 shell 为 zsh
+chsh -s "$(command -v zsh)"
+
+# 提示用户手动重启 shell
 echo '##################'
-echo 'finally,you need run "source ~/.zshrc" mannually now..'
-echo '##################'
+echo 'Installation complete!'
+echo 'Please restart your terminal or run "exec zsh" to apply changes.'
+echo '##################' 
